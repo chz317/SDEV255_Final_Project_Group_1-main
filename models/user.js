0c8283be48a7cd1410562aecb83bd4c12c9713e4
Joseph Cheesebourough <joseph.cheeseb@gmail.com>
2024-07-19 20:51:27 -0400

    node modules

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['student', 'teacher'],
+        required: true
+    }
+}, { timestamps: true });
+
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 12);
+    }
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
